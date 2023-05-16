9e4341d817036fb67856e04d6b0842e6fc9feb09
ammar <ammar.ali.haidar.1990@gmail.com>
2023-05-16 13:17:57 +0300

    work

--- controllers/order.controllers.js
+++ controllers/order.controllers.js
@@ -2,6 +2,18 @@ import { createError } from "../utils/createError.js";
 import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
 import mongoose from "mongoose";
+import Stripe from 'stripe'
+
+
+
+
+
+
+
+
+
+
+
 
 export const createOrder = async (req, res, next) => {
   const valid = mongoose.Types.ObjectId.isValid(req.params.gigId);
@@ -9,6 +21,8 @@ export const createOrder = async (req, res, next) => {
   try {
     const gig =await  Gig.findById(req.params.gigId);
     if (!gig) return next(createError(404, "no such gig"));
+
+    if(req.isSeller) return next(createError(404,'sellers can not buy gigs'))
 
     const newOrder = await Order.create({
       gigId: gig._id,
@@ -32,10 +46,7 @@ export const getOrders = async (req, res, next) => {
 
 try {
 
-    const orders = await Order.find({
-        ...(req.isSeller ? {sellerId:req.userId} : {buyerId:req.userId} ),
-        isCompleted:true
-    })
+    const orders = await Order.find(req.isSeller ? {sellerId:req.userId}:{buyerId:req.userId})
 
     res.status(200).json(orders)
     
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../middleware/jwt.js'
-import { createOrder,getOrders } from '../controllers/order.controllers.js'
+import {getOrders,createOrder } from '../controllers/order.controllers.js'
 
 const route = express.Router()
 
@@ -8,5 +8,6 @@ const route = express.Router()
 
 route.post('/:gigId',verifyToken,createOrder)
 route.get('/',verifyToken,getOrders)
+
 
 export default route
